872bba55a57ec2f4889a31ccaecac320cdd4c44f
FreeBird-spectre <sethabir699@gmail.com>
2025-10-03 19:52:57 +0530

    Update Bubble Sort.cpp

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -3,32 +3,26 @@
 //PRN-24070123003
 
 #include <iostream>
+#include <algorithm> 
 using namespace std;
 
 void bubbleSort(int arr[], int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
+    for (int i = 0; i < size - 1; i++)
+        for (int j = 0; j < size - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
 }
 
 int main() {
-    int arr[] = {5, 1, 4, 2, 8};
+    int arr[] = {56, 1, 69, 2, 28};
     int size = sizeof(arr) / sizeof(arr[0]);
 
     bubbleSort(arr, size);
 
-    for (int i = 0; i < size; i++)
-        cout << arr[i] << " ";
-
-    return 0;
+    for (int x : arr) cout << x << " ";
 }
 
-1 2 4 5 8 
+
+1 2 28 56 69 
 
 === Code Execution Successful ===
